f315753ca054712db8687e11e43033fc27138fbd
BrodyLinux <Frostwizard7987@gmail.com>
2022-06-08 19:50:06 -0500

    Add components starter files

---
+++ src/components/Lists.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Lists = props => {
+  return (
+    <div>Lists</div>
+  )
+}
+
+Lists.propTypes = {
+    
+}
+
+export default Lists
---
+++ src/components/Todos.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Todos = props => {
+  return (
+    <div>Todos</div>
+  )
+}
+
+Todos.propTypes = {
+    
+}
+
+export default Todos
