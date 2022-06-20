2b07a2fd123bd6ad4c4c32bda9bb2c12b9c1140e
Brody <brodypen7987@gmail.com>
2022-06-19 20:22:39 -0500

    Remove date and finished form

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { ThemeProvider, createTheme } from "@mui/material/styles"
 import Header from "./components/Header"
 import { Paper, Box } from "@mui/material";
 import TodoForm from "./components/TodoForm";
-
 function App() {
   const darkTheme = createTheme({
     palette: {
@@ -12,11 +11,20 @@ function App() {
     },
   });
   const [todos, setTodos] = useState([{id: 1, title: 'Todo', description: 'Info', complete: false}]);
+  const OnTodoSubmit = (id, name, description, complete) => {
+    const newTodo = {};
+    newTodo["id"] = id;
+     newTodo["title"] = name;
+      newTodo["description"] = description;
+       newTodo["complete"] = complete;
+    setTodos(oldTodos => [...oldTodos, newTodo]);
+    console.table(todos);
+  }
   return (
     <ThemeProvider theme={darkTheme}>
       <Box bgcolor={"background.default"} color={"text.primary"}>
         <Header/>
-        <TodoForm/>
+        <TodoForm onClick={OnTodoSubmit} />
       </Box>
     </ThemeProvider>
 
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,16 +1,16 @@
-import React from 'react'
+import React, {useState, useId} from "react";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import Dialog from "@mui/material/Dialog";
 import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
-import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
-import DatePicker from './DatePicker';
-
-const TodoForm = () => {
+import { FormControlLabel, FormGroup, Switch } from "@mui/material";
+const TodoForm = ({ onClick }) => {
   const [open, setOpen] = React.useState(false);
-
+  const [name, setName] = React.useState("");
+  const [desc, setDesc] = React.useState("");
+  const [complete, setComplete] = React.useState(false);
   const handleClickOpen = () => {
     setOpen(true);
   };
@@ -18,6 +18,7 @@ const TodoForm = () => {
   const handleClose = () => {
     setOpen(false);
   };
+  const id = useId();
 
   return (
     <div>
@@ -27,16 +28,33 @@ const TodoForm = () => {
       <Dialog open={open} onClose={handleClose}>
         <DialogTitle>New Task</DialogTitle>
         <DialogContent>
-          <TextField id="name" label="Name" variant="filled" />
-          <TextField id="details" label="details" variant="filled" />
-          <DatePicker/>
+          <TextField
+            id="name"
+            label="Name"
+            variant="filled"
+            onChange={(e) => setName(e.target.value)}
+          />
+          <TextField
+            id="Description"
+            label="Description"
+            variant="filled"
+            onChange={(e) => setDesc(e.target.value)}
+          />
+          <FormGroup>
+            <FormControlLabel
+              control={<Switch />}
+              label="Complete?"
+              color="secondary"
+              onChange={(e) => e.target.value === 'on' ? setComplete(true) : setComplete(false)}
+            ></FormControlLabel>
+          </FormGroup>
         </DialogContent>
         <DialogActions>
-          <Button onClick={handleClose}>Submit</Button>
+          <Button onClick={() => onClick(id, name, desc, complete)}>Submit</Button>
         </DialogActions>
       </Dialog>
     </div>
   );
-}
+};
 
-export default TodoForm
+export default TodoForm;
